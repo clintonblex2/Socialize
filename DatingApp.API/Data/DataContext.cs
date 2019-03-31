ac01ffbf758514d9cbc94adefb0021f45abb318d
smart <clintonblex2@gmail.com>
2019-03-31 05:29:11 +0100

    Initial commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    // DataContext is a derived class of DbContext
+    public class DataContext : DbContext
+    {
+        // create a constructor
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        // tell entity framework about our entity i.e Value model class
+        public DbSet<Value> Values { get; set; }
+    }
+}
